64e424cdb4a6688e55c619f74f1367f86fe32b7a
NGUYỄN HỮU ĐĂNG <ducbmt2009@gmail.com>
2022-12-16 20:10:44 +0700

    cors

--- src/app.js
+++ src/app.js
@@ -9,10 +9,13 @@ const path = require('path')
 
 const errorMiddleware = require('./middlewares/errors')
 
+const cors = require('cors')
+
 // Setting up config file 
 if (process.env.NODE_ENV !== 'PRODUCTION') require('dotenv').config({ path: 'src/config/config.env' })
 // dotenv.config({ path: 'backend/config/config.env' })
 
+app.use(cors());
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser())
