f537d677f51ef1df9ee9115f2d7cad5dce01df4a
NGUYỄN HỮU ĐĂNG <ducbmt2009@gmail.com>
2022-12-18 18:24:23 +0700

    fix

--- src/app.js
+++ src/app.js
@@ -15,12 +15,11 @@ const cors = require('cors')
 if (process.env.NODE_ENV !== 'PRODUCTION') require('dotenv').config({ path: 'src/config/config.env' })
 // dotenv.config({ path: 'backend/config/config.env' })
 
-app.use(cors());
+app.use(cors({credentials: true, origin: process.env.FRONTEND_URL}));
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser())
 app.use(fileUpload());
-
 
 // Import all routes
 const products = require('./routes/product');
